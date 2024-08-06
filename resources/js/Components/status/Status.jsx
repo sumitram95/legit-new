743330632220f71f32681fcd481c54ff38a0b312
Sumit <sumit@sysqube.com.np>
2024-08-06 12:51:38 +0545

    chore: add Link on logo, add checked button for status for all

--- resources/js/Components/status/Status.jsx
+++ resources/js/Components/status/Status.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import StatusLists from "./StatusLists";
 
 export default function Status({ className = "", ...props }) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    function handleShowAllChecked() {
+        setIsChecked(!isChecked);
+    }
     return (
         <div className="flex gap-5 border p-2 rounded-md flex-wrap">
             {StatusLists.map((StatusList, index) => (
@@ -10,11 +15,24 @@ export default function Status({ className = "", ...props }) {
                         {...props}
                         type="checkbox"
                         id={StatusList}
+                        checked={isChecked}
                         className={`rounded ${StatusList} focus:ring-0 ${className}`}
                     />
-                    <label htmlFor={StatusList} className=" capitalize">{StatusList}</label>
+                    <label htmlFor={StatusList} className=" capitalize">
+                        {StatusList}
+                    </label>
                 </div>
             ))}
+
+            <div>
+                <button
+                    type="button"
+                    className=" text-blue-400 hover:underline"
+                    onClick={handleShowAllChecked}
+                >
+                    Show all
+                </button>
+            </div>
         </div>
     );
 }
