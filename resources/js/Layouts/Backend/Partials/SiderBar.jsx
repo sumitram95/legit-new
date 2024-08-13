b4677605ec1c06370ec1da4e66e4a18343c1bc35
Sumit <sumit@sysqube.com.np>
2024-08-13 12:46:40 +0545

    feat: add news sider

--- resources/js/Layouts/Backend/Partials/SiderBar.jsx
+++ resources/js/Layouts/Backend/Partials/SiderBar.jsx
@@ -63,17 +63,50 @@ export default function SiderBar() {
                             )} flex items-center p-2 rounded-lg hover:bg-blue-100 group`}
                         >
                             <svg
-                                className="w-5 h-5 text-gray-500 transition duration-75"
-                                aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="#4E87D3"
-                                viewBox="0 0 22 21"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-6"
                             >
-                                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 0 1-1.161.886l-.143.048a1.107 1.107 0 0 0-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 0 1-1.652.928l-.679-.906a1.125 1.125 0 0 0-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 0 0-8.862 12.872M12.75 3.031a9 9 0 0 1 6.69 14.036m0 0-.177-.529A2.25 2.25 0 0 0 17.128 15H16.5l-.324-.324a1.453 1.453 0 0 0-2.328.377l-.036.073a1.586 1.586 0 0 1-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 0 1-5.276 3.67m0 0a9 9 0 0 1-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25"
+                                />
                             </svg>
+
                             <span className="ms-3 text-sm text-gray-700 capitalize">
                                 AI Plolicy Tracker
+                            </span>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                            href={route("backend.ai_policy_tracker.index")}
+                            className={`${isActiveUrl(
+                                route("backend.ai_policy_tracker.index")
+                            )} flex items-center p-2 rounded-lg hover:bg-blue-100 group`}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#4E87D3"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="size-6"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
+                                />
+                            </svg>
+
+                            <span className="ms-3 text-sm text-gray-700 capitalize">
+                                News
                             </span>
                         </Link>
                     </li>
