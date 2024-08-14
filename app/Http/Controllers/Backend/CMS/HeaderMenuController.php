c8cc585e15aee47558b2089add8a31e89d6b9008
Sumit <sumit@sysqube.com.np>
2024-08-14 12:19:28 +0545

    chore: ui of logo,navbar,aipolicytracker

--- app/Http/Controllers/Backend/CMS/HeaderMenuController.php
+++ app/Http/Controllers/Backend/CMS/HeaderMenuController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Backend\CMS;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class HeaderMenuController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render("Backend/Cms/HeaderMenu");
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'logo' => 'sometimes|nullable|mimes:png,jpg,gif',
+            'navBars.*' => 'sometimes|nullable',
+            'aiPolicyTrackeData.*' => 'sometimes|nullable'
+        ]);
+        dd($validated);
+    }
 }
