7971afc41f43a09c4a682640e70e701aebc9b3c1
suraj suwal <surajsuwa@gmail.com>
2024-08-19 17:59:27 +0545

    fix: fix single ai policy tracker page

--- resources/js/Pages/Frontend/AiPolicyTracker/components/SingleAiInfo.jsx
+++ resources/js/Pages/Frontend/AiPolicyTracker/components/SingleAiInfo.jsx
@@ -1,58 +1,71 @@
 import { Link } from "@inertiajs/react";
 import React from "react";
 
-export default function SingleAiInfo() {
+export default function SingleAiInfo({ aiPolicyTrackerdetail }) {
+    // console.log(aiPolicyTrackerdetail);
+
+    const {
+        ai_policy_name,
+        technology_partners,
+        announcement_year,
+        governing_body,
+        governance_structure,
+        status,
+        country,
+        description,
+        whitepaper_document_link,
+    } = aiPolicyTrackerdetail;
+
     return (
         <div className="bg-white rounded-md py-3">
-            <div className=" border-b-2 pb-4 px-5">
+            <div className="border-b-2 pb-4 px-5">
                 <div>
                     <p className="font-bold text-primary text-lg">
-                        Act AI (AIPT) <i className="fa-regular fa-star"></i>
+                        {ai_policy_name ?? ''} <i className="fa-regular fa-star"></i>
                     </p>
                 </div>
                 <div className="flex gap-5 mt-3 text-sm">
-                    <p className="text-sm text-primary">Act AI</p>
+                    <p className="text-sm text-primary">{technology_partners ?? ''}</p>
                 </div>
             </div>
 
             <div>
                 <div className="px-5 mt-4">
-                    <p className=" capitalize text-xs">Announcement Year</p>
-                    <p className="text-gray-600">2024</p>
+                    <p className="capitalize text-xs">Announcement Year</p>
+                    <p className="text-gray-600">{announcement_year ?? ''}</p>
                 </div>
 
                 <div className="px-5 mt-4">
-                    <p className=" capitalize text-xs">Announcement Year</p>
-                    <p className="text-gray-600">2024</p>
+                    <p className="capitalize text-xs">Governing Body</p>
+                    <p className="text-gray-600">{governing_body ?? ''}</p>
                 </div>
-
                 <div className="px-5 mt-4">
-                    <p className=" capitalize text-xs">Announcement Year</p>
-                    <p className="text-gray-600">2024</p>
+                    <p className="capitalize text-xs">Governance Structure</p>
+                    <p className="text-gray-600">{governance_structure ?? ''}</p>
                 </div>
-
                 <div className="px-5 mt-4">
-                    <p className=" capitalize text-xs">Announcement Year</p>
-                    <p className="text-gray-600">2024</p>
+                    <p className="capitalize text-xs">Status</p>
+                    <p className="text-gray-600">{status?.name ?? ''}</p>
                 </div>
-
                 <div className="px-5 mt-4">
-                    <p className=" capitalize text-xs">Announcement Year</p>
-                    <p className="text-gray-600">2024</p>
+                    <p className="capitalize text-xs">Country</p>
+                    <p className="text-gray-600">{country?.name ?? ''}</p>
+                </div>
+                <div className="px-5 mt-4">
+                    <p className="capitalize text-xs">Description</p>
+                    <p className="text-gray-600">{description ?? ''}</p>
                 </div>
 
                 <div className="px-5 mt-4 border-t-2 pt-4">
                     <p className="text-xs">Link to announcement</p>
                     <a
-                        href={
-                            "https://x.com/ReserveBankZIM/status/1590560608994623489"
-                        }
+                        href={whitepaper_document_link ?? '#'}
                         target="_blank"
                         rel="noopener noreferrer"
                         className="text-gray-600"
                     >
                         <i className="fa-solid fa-link text-xs"></i>
-                        <span className="ml-1 hover">twiter.com</span>
+                        <span className="ml-1 hover">{whitepaper_document_link ?? 'No link available'}</span>
                     </a>
                 </div>
             </div>
