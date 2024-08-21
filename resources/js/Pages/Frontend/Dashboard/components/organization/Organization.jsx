703f7e2157c6b19dad0f6f63d6c9e92bee7f1598
suraj suwal <surajsuwa@gmail.com>
2024-08-21 16:41:50 +0545

    fix: fix client slider on landing page

--- resources/js/Pages/Frontend/Dashboard/components/organization/Organization.jsx
+++ resources/js/Pages/Frontend/Dashboard/components/organization/Organization.jsx
@@ -3,6 +3,11 @@ import { Link } from "@inertiajs/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import artificalLogo from "@/assets/images/clients/artifical.png";
+import mindBridgeLogo from "@/assets/images/clients/mind-bridge.png";
+import morpheusLogo from "@/assets/images/clients/morpheus.png";
+import neverMindLogo from "@/assets/images/clients/never-mind.png";
+import nextMindLogo from "@/assets/images/clients/nexmind.png";
 
 export default function Organization({ organizationLogo }) {
     const org = [1, 2, 3, 4];
@@ -28,13 +33,39 @@ export default function Organization({ organizationLogo }) {
                 </div>
             </div>
             <Slider {...settings}>
-                {org.map((data, index) => (
+                {/* {org.map((data, index) => (
                     <div className="mt-3" key={index}>
                         <Link hre={"/logo"} className=" w-fit">
                             <img src={organizationLogo} alt="" />
                         </Link>
                     </div>
-                ))}
+                ))} */}
+
+                <div className="mt-3">
+                    <Link hre={"/logo"} className=" w-fit">
+                        <img src={artificalLogo} alt="" />
+                    </Link>
+                </div>
+                <div className="mt-3">
+                    <Link hre={"/logo"} className=" w-fit">
+                        <img src={mindBridgeLogo} alt="" />
+                    </Link>
+                </div>
+                <div className="mt-3">
+                    <Link hre={"/logo"} className=" w-fit">
+                        <img src={morpheusLogo} alt="" />
+                    </Link>
+                </div>
+                <div className="mt-3">
+                    <Link hre={"/logo"} className=" w-fit">
+                        <img src={neverMindLogo} alt="" />
+                    </Link>
+                </div>
+                <div className="mt-3">
+                    <Link hre={"/logo"} className=" w-fit">
+                        <img src={nextMindLogo} alt="" />
+                    </Link>
+                </div>
             </Slider>
         </div>
     );
