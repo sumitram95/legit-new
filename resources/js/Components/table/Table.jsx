4ce7fbbcdf2589e0f39213fc64c98dfbb5413ce6
suraj suwal <surajsuwa@gmail.com>
2024-08-22 13:23:37 +0545

    fix: fix watch list on dashboard

--- resources/js/Components/table/Table.jsx
+++ resources/js/Components/table/Table.jsx
@@ -12,7 +12,7 @@ export default function Table({
 }) {
     const hasData = Array.isArray(tableData) && tableData.length > 0;
 
-    console.log(watchListIds);
+    // console.log(watchListIds);
     return (
         <div className="relative overflow-x-auto px-5 mt-5">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500">
