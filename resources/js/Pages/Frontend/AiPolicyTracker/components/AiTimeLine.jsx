57905e4ede3fec738c5c0c4310998809b1716b5c
suraj suwal <surajsuwa@gmail.com>
2024-08-26 16:10:46 +0545

    fix: fix and update timeline font

--- resources/js/Pages/Frontend/AiPolicyTracker/components/AiTimeLine.jsx
+++ resources/js/Pages/Frontend/AiPolicyTracker/components/AiTimeLine.jsx
@@ -37,14 +37,14 @@ export default function AiTimeLine({ timeLines = [] }) {
                             </h3>
 
                             <div className="mb-2 bg-blue-100 px-5 py-2 rounded-md">
-                                <p className="text-sm font-normal text-blue-500 flex items-center gap-2">
+                                <p className=" text-blue-500 flex items-center gap-1 font-bold text-xs">
                                     <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="arrow repeat" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-arrow-repeat b-icon bi"><g><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"></path><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"></path></g></svg>
                                     {timeLine.activity_name}
                                 </p>
                             </div>
                             <div className="mb-2 bg-blue-100 px-5 py-2 rounded-md">
                                 <p
-                                    className="text-sm font-normal text-blue-500"
+                                    className="text-xs font-normal text-blue-500"
                                     dangerouslySetInnerHTML={{ __html: timeLine.description }}
                                 ></p>
 
