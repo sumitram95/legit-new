128af529ade50d4f647be38a8516be837a7b290f
suraj suwal <surajsuwa@gmail.com>
2024-08-28 09:57:13 +0545

    fix: fix stroage link

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 require __DIR__ . "/frontend/web.php";
 require __DIR__ . '/auth.php';
 require __DIR__ . "/backend/web.php";
+
+
+//-- Storage link
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return "Storage link created!";
+})->middleware(['auth']);
+
+//-- Cache
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    return "Cache cleared!";
+})->middleware(['auth']);
