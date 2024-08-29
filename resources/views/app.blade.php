2aa85b1e10c95a79f4a4c9189de32a708d8b6a82
suraj suwal <surajsuwa@gmail.com>
2024-08-29 12:57:16 +0545

    fix: fix meta detail

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,18 +10,20 @@
     <meta name="twitter:title" content="Global Artificial Intelligence (AI) Policy Tracker">
     <meta name="twitter:description"
         content="Stay informed with the latest AI policy insights, updates, and trends. Explore comprehensive resources on AI ethics, legislation, compliance, frameworks, research, development, implementation, and more. Join our community for discussions, workshops, conferences, and collaborations on AI governance and regulations. Get access to policy guidelines, standards, best practices, and strategic roadmaps to navigate the evolving landscape of artificial intelligence policies.">
-    <meta name="twitter:image" content="https://aipolicytracker.org/build/assets/loginpage-DU1Dibxo.jpg">
+    <meta name="twitter:image" content="{{ asset('aipolicytracker-logo.jpg') }}">
     <meta name="og:title" content="Global Artificial Intelligence (AI) Policy Tracker">
     <meta name="og:site_name" content="Global Artificial Intelligence (AI) Policy Tracker">
     <meta name="og:type" content="website">
     <meta name="og:description"
         content="Stay informed with the latest AI policy insights, updates, and trends. Explore comprehensive resources on AI ethics, legislation, compliance, frameworks, research, development, implementation, and more. Join our community for discussions, workshops, conferences, and collaborations on AI governance and regulations. Get access to policy guidelines, standards, best practices, and strategic roadmaps to navigate the evolving landscape of artificial intelligence policies.">
-    <meta name="og:image" content="https://aipolicytracker.org/build/assets/loginpage-DU1Dibxo.jpg">
+    <meta name="og:image" content="{{ asset('aipolicytracker-logo.jpg') }}">
     <meta name="description"
         content="Stay informed with the latest AI policy insights, updates, and trends. Explore comprehensive resources on AI ethics, legislation, compliance, frameworks, research, development, implementation, and more. Join our community for discussions, workshops, conferences, and collaborations on AI governance and regulations. Get access to policy guidelines, standards, best practices, and strategic roadmaps to navigate the evolving landscape of artificial intelligence policies.">
     <meta name="keywords"
         content="AI ethics, AI legislation, AI compliance, AI policy framework, AI policy research, AI policy development, AI policy implementation, AI policy review, AI policy enforcement, AI policy strategy, AI policy roadmap, AI policy guidelines, AI policy standards, AI policy best practices, AI policy recommendations, AI policy white papers, AI policy briefings, AI policy workshops, AI policy conferences, AI policy seminars, AI policy forums, AI policy networks, AI policy partnerships, AI policy collaborations, AI policy initiatives, AI policy programs, AI policy projects, AI policy grants, AI policy funding, AI policy investments, AI policy impact, AI policy outcomes, AI policy metrics, AI policy indicators, AI policy benchmarks, AI policy assessments, AI policy evaluations, AI policy audits, AI policy reviews, AI policy updates">
 
+    <!-- Meta icon -->
+    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}">
 
     <title>Global Artificial Intelligence (AI) Policy Status</title>
 
@@ -29,9 +31,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Meta icon -->
-    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
