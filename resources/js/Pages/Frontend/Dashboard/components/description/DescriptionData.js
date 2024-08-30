7668f4fbf773dc2cd7020bacf7cb336336737930
suraj suwal <surajsuwa@gmail.com>
2024-08-30 13:54:50 +0545

    fix: fix design

--- resources/js/Pages/Frontend/Dashboard/components/description/DescriptionData.js
+++ resources/js/Pages/Frontend/Dashboard/components/description/DescriptionData.js
@@ -8,14 +8,6 @@ const DescriptionData = [
         title: "Countries that have conducted first explanatory AI Policy research.",
     },
     {
-        name: "Whitepaper",
-        title: "Countries that are in an advanced research stage and have published a Whitepaper.",
-    },
-    {
-        name: "Pilot",
-        title: "Countries piloting AI policy in a real environment with a limited number of partie.",
-    },
-    {
         name: "Development",
         title: "Countries piloting AI policy in a real environment with a limited number of partie.",
     },
@@ -23,10 +15,7 @@ const DescriptionData = [
         name: "Launched",
         title: "Countries that officially launched an AI policy.",
     },
-    {
-        name: "Cancelled",
-        title: "Countries that canceled or decommissioned an AI Policy.",
-    },
+
 ];
 
 export default DescriptionData;
