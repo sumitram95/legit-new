b95b23693cc3cc96be1add83eadd1d777b42b5ac
Sumit <sumit@sysqube.com.np>
2024-09-10 15:06:42 +0545

    fix: commented AiPolicyTrackerSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CountrySeeder::class);
         $this->call(AdminSeeder::class);
         $this->call(StatusSeeder::class);
-        $this->call(AiPolicyTrackerSeeder::class);
+        // $this->call(AiPolicyTrackerSeeder::class);
     }
 }
