83cc0f1f73c24fbedcf6916369965e91a49bbe00
Sumit <sumit@sysqube.com.np>
2024-09-11 18:28:35 +0545

    WIP:in progress

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\AiPolicyTracker;
 use App\Models\News;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -28,6 +30,49 @@ class DashboardController extends Controller
 
         $data['aiPolicyCount'] = AiPolicyTracker::count();
         $data['newsCount'] = News::count();
+
+
+
+        $currentYear = Carbon::now()->year;
+        $upcomingYear = $currentYear + 1;
+
+        $months = [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10',
+            '11',
+            '12'
+        ];
+
+        // Fetch customer counts for all months
+        $graphCount['PaymentCount'] = AiPolicyTracker::selectRaw('MONTH(created_at) as month, COUNT(*) as value')
+            ->whereYear('created_at', '>=', $currentYear)
+            ->whereYear('created_at', '<=', $upcomingYear)
+            ->groupBy(DB::raw('MONTH(created_at)'))
+            ->orderBy(DB::raw('MONTH(created_at)'), 'asc')
+            ->pluck('value', 'month')
+            ->toArray();
+
+            // dd($graphCount['PaymentCount']);
+
+        // Prepare data array with counts for all months
+        $graphData = [];
+        $mothNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        foreach ($months as $index => $month) {
+            $graphData[] = [
+                'month' => $mothNames[$index],
+                'PaymentCount' => $graphCount['PaymentCount'][$month] ?? 0,
+            ];
+        }
+
+        dd($graphCount);
 
 
 
--- resources/js/Pages/Backend/Dashboard.jsx
+++ resources/js/Pages/Backend/Dashboard.jsx
@@ -1,6 +1,7 @@
 import Layout from "@/Layouts/Backend/Layout";
 import { Head } from "@inertiajs/react";
 import React from "react";
+import GraphChart from "./Services/GraphChart";
 
 export default function Dashboard({ data }) {
     return (
@@ -81,25 +82,9 @@ export default function Dashboard({ data }) {
                         </div>
                     </div>
                 </div>
-                {/* <div className="flex items-center justify-center h-48 mb-4 rounded bg-white">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg
-                            className="w-3.5 h-3.5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 18 18"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M9 1v16M1 9h16"
-                            />
-                        </svg>
-                    </p>
-                </div> */}
+                <div className="mb-4 rounded bg-white">
+                   <GraphChart />
+                </div>
                 <div className="grid grid-cols-2 gap-4 mb-4">
                     <div className="rounded bg-white min-h-28 py-5">
                         <div className="flex items-center justify-between px-5 mb-3">
