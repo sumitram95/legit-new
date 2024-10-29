f8cd13de451c7571b176efd7e7390fbe78443448
Sumit <sumit@sysqube.com.np>
2024-10-29 09:12:46 +0545

    feat: add graph data in single legit page(frontend)

--- resources/js/Pages/Frontend/AiPolicyTracker/SingleAlert.jsx
+++ resources/js/Pages/Frontend/AiPolicyTracker/SingleAlert.jsx
@@ -4,10 +4,13 @@ import React from "react";
 import AiNews from "./Components/AiNews";
 import SingleAiInfo from "./Components/SingleAiInfo";
 import AiTimeLine from "./Components/AiTimeLine";
+import GraphData from "./Components/GraphData";
 
 export default function SingleAlert({
-    aiPolicyTrackerWithRelatedNews,
-    latestDateOfUpdateAiPolicyTracker = null,
+    // aiPolicyTrackerWithRelatedNews,
+    // latestDateOfUpdateAiPolicyTracker = null,
+    // graphData
+    data,
 }) {
     return (
         <AppLayout>
@@ -21,15 +24,17 @@ export default function SingleAlert({
                 </Link>
             </div>
             <div className="md:flex flex-row gap-x-4 content-wrapper xl:mt-0 lg:mt-0 md:relative md:top-[-60px]">
-                <div className="w-full md:w-1/4">
+                <div className="w-full md:w-1/3">
                     <SingleAiInfo
-                        aiPolicyTrackerdetail={aiPolicyTrackerWithRelatedNews}
+                        aiPolicyTrackerdetail={data.aiPolicyTrackerWithRelatedNews}
                     />
                 </div>
-                <div className="w-full mt-5 md:mt-0 md:w-2/4">
-                    <AiNews newsLists={aiPolicyTrackerWithRelatedNews.news} />
+                <div className="w-full mt-5 md:mt-0 md:w-full">
+                    {/* <AiNews newsLists={aiPolicyTrackerWithRelatedNews.news} />
+                     */}
+                    <GraphData graphData={data.graphData} currentYear={data.currentYear} upcomingYear={data.upcomingYear}/>
                 </div>
-                <div className="w-full mt-5 md:mt-0 md:w-1/4">
+                {/* <div className="w-full mt-5 md:mt-0 md:w-1/4">
                     <AiTimeLine
                         timeLines={
                             aiPolicyTrackerWithRelatedNews.a_i_policy_activity_logs
@@ -38,51 +43,8 @@ export default function SingleAlert({
                             latestDateOfUpdateAiPolicyTracker
                         }
                     />
-                </div>
-            </div>
-            {/* <div className="content-wrapper mt-5"> */}
-            {/* <div className="flex gap-5 mt-3 text-sm mb-5">
-                        <Link
-                            href="/"
-                            className=" text-sm flex gap-2 hover:underline"
-                        >
-                            <span>
-                                <i className="fa-regular fa-circle-left"></i>
-                            </span>
-                            <span>Go to back</span>
-                        </Link>
-                    </div> */}
-            {/* Flexbox on large screens (lg and xl) */}
-            {/* <div className="w-full block lg:flex gap-8"> */}
-            {/* Single AI Info */}
-            {/* <div className="w-full lg:max-w-[25%]">
-                            <SingleAiInfo
-                                aiPolicyTrackerdetail={
-                                    aiPolicyTrackerWithRelatedNews
-                                }
-                            />
-                        </div> */}
-
-            {/* Ai News */}
-            {/* <div className="w-full mt-5 lg:mt-0">
-                            <AiNews
-                                newsLists={aiPolicyTrackerWithRelatedNews.news}
-                            />
-                        </div> */}
-
-            {/* Ai Timeline */}
-            {/* <div className="w-full mt-5 lg:mt-0 lg:max-w-[25%]">
-                            <AiTimeLine
-                                timeLines={
-                                    aiPolicyTrackerWithRelatedNews.a_i_policy_activity_logs
-                                }
-                                latestDateOfUpdateAiPolicyTracker={
-                                    latestDateOfUpdateAiPolicyTracker
-                                }
-                            />
-                        </div>
-                    </div>
                 </div> */}
+            </div>
         </AppLayout>
     );
 }
